db4286ef3811d926ffe122ca68d39dc97370cc8c
braulioboada <brathlowiis@gmail.com>
2024-04-30 20:49:17 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,25 @@
+function calculate() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var operation = document.getElementById('operation').value;
+    var result;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            result = "Invalid operation";
+    }
+
+    document.getElementById('result').innerHTML = "Resultado: " + result;
+}
